6fb9582637de828faf81ce5ebd42a62970af6477
Erenthos <erenthos@gmail.com>
2025-11-12 19:23:36 +0530

    Update pdfGenerator.ts

--- src/lib/pdfGenerator.ts
+++ src/lib/pdfGenerator.ts
@@ -1,1 +1,8 @@
+import html_to_pdf from "html-pdf-node";
+
+export async function generatePDF(html: string, filename: string) {
+  const file = { content: html };
+  const pdfBuffer = await html_to_pdf.generatePdf(file, { format: "A4" });
+  return pdfBuffer;
+}
 
